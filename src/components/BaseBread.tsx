330b4e56d4ad1c28ecd7a46fc0ca5bd2d48e3b29
Unathi Lenkoe <unathilenkoe@gmail.com>
2024-07-21 13:11:17 +0200

    adding base bread of the burger and styling

---
+++ src/components/BaseBread.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/BaseBread.css'
+
+const BaseBread = ({ size }) => {
+  return (
+    <div className={`base-bread ${size}`}>
+    </div>
+  );
+};
+
+export default BaseBread;
